023a4477a6372c91614d0ae0c9db23f5fdfa8371
taoxiangyu <taoxiangyujava@163.com>
2024-06-13 17:07:02 +0800

    3

--- src/main/java/MybatisApp.java
+++ src/main/java/MybatisApp.java
@@ -7,6 +7,6 @@ public class MybatisApp {
     public static void main(String[] args) {
         System.out.println("1243132");
 
-        System.out.println("22222");
+        System.out.println("333333");
     }
 }
