14c448c80ba4551e432b8d7b03faab3fcf74f04c
taoxiangyu <taoxiangyujava@163.com>
2024-06-13 16:51:52 +0800

    1

---
+++ src/main/java/MybatisApp.java
@@ -0,0 +1,10 @@
+/**
+ * @Author : taoxy
+ * @Date: 2024/6/13 16:49
+ * @Description :
+ */
+public class MybatisApp {
+    public static void main(String[] args) {
+        System.out.println("1243");
+    }
+}
